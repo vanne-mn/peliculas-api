2f4830f440d645f864a2f0d9cee4a92393d00408
Vannesa Moncada <tucorreo@ejemplo.com>
2026-03-25 05:43:37 -0500

    debug server

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+console.log("🔥 SERVER CORRECTO EJECUTÁNDOSE 🔥");
+
 require('dotenv').config();
 const app = require('./src/app');
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en puerto ${PORT}`);
+  console.log(`Servidor corriendo en puerto ${PORT}`);
 });
