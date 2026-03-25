4365db5eb647e4e79366e952ee3c12d441d4fe4a
Vannesa Moncada <tucorreo@ejemplo.com>
2026-03-25 16:01:08 -0500

    conexion con mysql_url

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,7 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
+const connection = mysql.createConnection(process.env.DATABASE_URL);
 
 connection.connect((err) => {
     if (err) {
